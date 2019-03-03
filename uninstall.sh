27fd300b02850f0ed274a84d3b0d132486195630
ogabriel <gabrieloliver8991@gmail.com>
2019-03-03 00:11:40 -0300

    adding the install and uninstall files

---
+++ uninstall.sh
@@ -0,0 +1,16 @@
+cd
+
+# erase the source versions
+rm .vimrc
+rm .vimrc.plugins
+rm .tmux.conf
+rm .tmux.conf.local
+
+# move all the originals back again
+mv dotfiles_backup/.vimrc
+mv dotfiles_backup/.vimrc.plugins
+mv dotfiles_backup/.tmux.conf
+mv dotfiles_backup/.tmux.conf.local
+
+# if still something there, probaly is gonna fail
+rmdir dotfiles_backup
