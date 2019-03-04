67d3246ed0eb3331b0831565e5b9bc3ef246711f
ogabriel <gabrieloliver8991@gmail.com>
2019-03-03 22:37:18 -0300

    implementing new configs at install

--- uninstall.sh
+++ uninstall.sh
@@ -7,10 +7,10 @@ rm .tmux.conf
 rm .tmux.conf.local
 
 # move all the originals back again
-mv dotfiles_backup/.vimrc
-mv dotfiles_backup/.vimrc.plugins
-mv dotfiles_backup/.tmux.conf
-mv dotfiles_backup/.tmux.conf.local
+mv dotfiles_backup/.vimrc ~/
+mv dotfiles_backup/.vimrc.plugins ~/
+mv dotfiles_backup/.tmux.conf ~/
+mv dotfiles_backup/.tmux.conf.local ~/
 
 # if still something there, probaly is gonna fail
 rmdir dotfiles_backup
