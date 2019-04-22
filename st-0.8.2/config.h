d3eb92a9c7794ecb8046d606e01e6d34e7fb7bbc
Gabriel <gabrieloliver8991@gmail.com>
2019-04-22 18:32:34 -0300

    chaging font inside st

--- st-0.8.2/config.h
+++ st-0.8.2/config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Mono:pixelsize=12:antialias=true:autohint=true";
-static int borderpx = 2;
+static char *font = "InconsolataLGC Nerd Font:pixelsize=12:antialias=true:autohint=true";
+static int borderpx = 10;
 
 /*
  * What program is execed by st depends of these precedence rules:
