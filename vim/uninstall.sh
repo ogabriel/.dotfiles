085585397ffc2969a650666c7ac4fce00845e97a
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2019-06-22 15:06:38 -0300

    move uninstall to respective folders

--- vim/uninstall.sh
+++ vim/uninstall.sh
@@ -2,17 +2,15 @@
 
 cd
 
+echo "restore backup files for vim"
+
 # erase the source versions
 rm .vimrc
 rm .vimrc.plugins
-rm .tmux.conf
-rm .tmux.conf.local
 
 # move all the originals back again
 mv dotfiles_backup/.vimrc ~/
 mv dotfiles_backup/.vimrc.plugins ~/
-mv dotfiles_backup/.tmux.conf ~/
-mv dotfiles_backup/.tmux.conf.local ~/
 
 # if still something there, probaly is gonna fail
 rmdir dotfiles_backup
