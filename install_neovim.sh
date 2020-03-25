32abd29296755cae6b29743a5a8f0011dfd30a2d
ogabriel <gabrieloliver8991@gmail.com>
2020-03-25 19:12:05 -0300

    add installer for neovim

---
+++ install_neovim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+touch ~/.config/nvim/init.vim
+
+echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after" >> ~/.config/nvim/init.vim
+echo "let &packpath = &runtimepath" >> ~/.config/nvim/init.vim
+echo "source ~/.vimrc" >> ~/.config/nvim/init.vim
