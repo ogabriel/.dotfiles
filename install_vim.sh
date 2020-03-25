bca3ee9ec4fcb176e47a53740dff174ee806f0f2
ogabriel <gabrieloliver8991@gmail.com>
2020-03-25 00:32:29 -0300

    add full installer

---
+++ install_vim.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# install ctags
+sudo apt install \
+    gcc make \
+    pkg-config autoconf automake \
+    python3-docutils \
+    libseccomp-dev \
+    libjansson-dev \
+    libyaml-dev \
+    libxml2-dev
+
+mkdir ~/opensource
+cd ~/opensource
+git clone git@github.com:universal-ctags/ctags.git
+
+cd ~/opensource/ctags
+./autogen.sh
+./configure
+sudo make
+sudo make install
+
+# link files
+ln -sf ~/.dotfiles/vim/vimrc .vimrc
+
+# link folder
+ln -s ~/.dotfiles/vim .vim
