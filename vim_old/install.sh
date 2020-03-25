05a0340ac169d4df1b3bcdc32fb4e322968e6ae9
ogabriel <gabrieloliver8991@gmail.com>
2020-03-25 00:35:48 -0300

    fix old vim

--- vim_old/install.sh
+++ vim_old/install.sh
@@ -10,5 +10,5 @@ mv .vimrc .dotfiles_backup
 mv .vimrc.plugins .dotfiles_backup
 
 # just adding a source to the files at HOME, to redirect to the dotfile configuration
-ln -sf ~/.dotfiles/vim/.vimrc .vimrc
-ln -sf ~/.dotfiles/vim/.vimrc.plugins .vimrc.plugins
+ln -sf ~/.dotfiles/vim_old/vimrc .vimrc
+ln -sf ~/.dotfiles/vim_old/vimrc.plugins .vimrc.plugins
