6d28033943c9ca468e9e23b7f5107fd72cd5e580
ogabriel <gabrieloliver8991@gmail.com>
2020-03-26 00:44:19 -0300

    fix calls on vim installer

--- install_vim.sh
+++ install_vim.sh
@@ -21,7 +21,7 @@ sudo make
 sudo make install
 
 # link files
-ln -sf ~/.dotfiles/vim/vimrc .vimrc
+ln -sf ~/.dotfiles/vim/vimrc ~/.vimrc
 
 # link folder
-ln -s ~/.dotfiles/vim .vim
+ln -s ~/.dotfiles/vim ~/.vim
