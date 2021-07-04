f31160e9f07ed2dc08e94fda497723c3f1ae2de3
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2021-07-04 17:52:46 -0300

    add email and name config to script

--- install_git.sh
+++ install_git.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+read -rp "github email: " email
+git config --global user.email $email
+
+read -rp "github name: " name
+git config --global user.name "$name"
+
 git config --global init.templatedir ~/.dotfiles/git/templates
 git config --global include.path ~/.dotfiles/git/.gitalias
