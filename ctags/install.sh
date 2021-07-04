68f948c180bceb9fa017893233e076e426aa6209
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2021-07-04 20:15:51 -0300

    move ctags script to install

--- ctags/install.sh
+++ ctags/install.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
+
+# dependencies
+sudo apt install \
+    gcc make \
+    pkg-config autoconf automake \
+    python3-docutils \
+    libseccomp-dev \
+    libjansson-dev \
+    libyaml-dev \
+    libxml2-dev
+
+# download
+mkdir -p ~/opensource
+cd ~/opensource
+git clone git@github.com:universal-ctags/ctags.git
+
+# install it
+cd ~/opensource/ctags
+./autogen.sh
+./configure
+sudo make
+sudo make install
+
+# link the current ctags
 ln -s ~/.dotfiles/ctags ~/.ctags.d
