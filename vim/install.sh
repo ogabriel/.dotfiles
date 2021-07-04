a85ade613f66b039c21b3216e054fdbce60b6f82
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2021-07-04 20:15:51 -0300

    move ctags script to install

--- vim/install.sh
+++ vim/install.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-# install ctags
-sudo apt install \
-    gcc make \
-    pkg-config autoconf automake \
-    python3-docutils \
-    libseccomp-dev \
-    libjansson-dev \
-    libyaml-dev \
-    libxml2-dev
-
-mkdir ~/opensource
-cd ~/opensource
-git clone git@github.com:universal-ctags/ctags.git
-
-cd ~/opensource/ctags
-./autogen.sh
-./configure
-sudo make
-sudo make install
-
 # install this for deoplete
 sudo apt install python3-pip
 pip3 install --user pynvim
