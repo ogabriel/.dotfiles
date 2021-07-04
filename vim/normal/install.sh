b6e0f6cbd3239b860924965830fa52136d76996a
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2021-07-04 20:11:48 -0300

    fix folder structure

--- vim/normal/install.sh
+++ vim/normal/install.sh
@@ -2,13 +2,11 @@
 # read this file carefuly, then, run the command:
 cd
 
-echo "add new configuration to vim"
-
 # creating the backup folder for the original ones
 mkdir -p ~/.dotfiles_backup
 mv ~/.vimrc ~/.dotfiles_backup
 mv ~/.vimrc.plugins ~/.dotfiles_backup
 
 # just adding a source to the files at HOME, to redirect to the dotfile configuration
-ln -sf ~/.dotfiles/vim_old/vimrc ~/.vimrc
-ln -sf ~/.dotfiles/vim_old/vimrc.plugins ~/.vimrc.plugins
+ln -sf ~/.dotfiles/vim/normal/vimrc ~/.vimrc
+ln -sf ~/.dotfiles/vim/normal/vimrc.plugins ~/.vimrc.plugins
