99a72d6527aad0dbaf017e7883a165b9ec5b7ba6
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2021-07-04 22:12:21 -0300

    just

--- tmux/install.sh
+++ tmux/install.sh
@@ -5,10 +5,10 @@ cd
 sudo apt install tmux
 
 # creating the backup folder for the original ones
-mkdir -p .dotfiles_backup
-mv .tmux.conf .dotfiles_backup
-mv .tmux.conf.local .dotfiles_backup
+mkdir -p ~/.dotfiles_backup
+mv ~/.tmux.conf ~/.dotfiles_backup
+mv ~/.tmux.conf.local ~/.dotfiles_backup
 
 # just adding a source to the files at HOME, to redirect to the dotfile configuration
-ln -sf ~/.dotfiles/tmux/.tmux.conf .tmux.conf
-ln -sf ~/.dotfiles/tmux/.tmux.conf.local .tmux.conf.local
+ln -sf ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/tmux/.tmux.conf.local ~/.tmux.conf.local
