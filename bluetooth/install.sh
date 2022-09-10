2a19ff7dd3090202ac540af12cd98fa303016b5c
ogabriel <gabrieloliver8991@gmail.com>
2022-09-09 21:43:46 -0300

    better install

--- bluetooth/install.sh
+++ bluetooth/install.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 
+# install pipewire
+sudo add-apt-repository ppa:pipewire-debian/pipewire-upstream
+sudo apt update
+sudo apt install pipewire
+sudo apt install libspa-0.2-bluetooth
+sudo apt install pipewire-audio-client-libraries
+
+# disable pulseaudio and enable pipewire
+systemctl --user daemon-reload
+systemctl --user --now disable pulseaudio.service pulseaudio.socket
+systemctl --user mask pulseaudio
+systemctl --user --now enable pipewire-media-session.service
+
+# remove things not needed
+sudo apt remove ofono
+sudo apt remove ofono-phonesim
+
+# copy configs
 cp ~/.dotfiles/bluetooth/bluez-hardware.conf /usr/share/spa-0.2/bluez5/
