10834aa27541c7bcc327de31377c47e7dce82c12
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2023-01-17 09:31:49 -0300

    rename tmux files

--- tmux/install.sh
+++ tmux/install.sh
@@ -10,5 +10,5 @@ mv ~/.tmux.conf ~/.dotfiles_backup
 mv ~/.tmux.conf.local ~/.dotfiles_backup
 
 # just adding a source to the files at HOME, to redirect to the dotfile configuration
-ln -sf ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ln -sf ~/.dotfiles/tmux/.tmux.conf.local ~/.tmux.conf.local
+ln -sf ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/tmux/tmux.conf.local ~/.tmux.conf.local
