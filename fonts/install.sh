a51dd877e5162239065b2b6dbf4bdaf3c735ccbf
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2023-01-18 08:13:44 -0300

    fix

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget -O temp/jetbrains_mono.zip https://download-cdn.jetbrains.com/fonts/JetBrainsMono-2.304.zip
-unzip temp/jetbrains_mono.zip -d temp/jetbrains_mono
+wget -O /tmp/jetbrains_mono.zip https://download-cdn.jetbrains.com/fonts/JetBrainsMono-2.304.zip
+unzip /tmp/jetbrains_mono.zip -d /tmp/jetbrains_mono
 
-mv jetbrains_mono/fonts /usr/share/fonts/
+sudo mv /tmp/jetbrains_mono/fonts /usr/share/fonts/
