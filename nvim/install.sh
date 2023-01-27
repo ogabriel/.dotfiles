1569bb9c54a629a30f9e9d4c8d9aa399c742940a
ogabriel <gabrieloliver8991@gmail.com>
2023-01-27 00:22:45 -0300

    update nvim install

--- nvim/install.sh
+++ nvim/install.sh
@@ -13,3 +13,11 @@ sha256sum /tmp/$FILENAME > /tmp/checksum
 sha256sum -c /tmp/checksum
 
 sudo dpkg -i /tmp/$FILENAME
+
+mkdir -p ~/.config
+mkdir -p ~/.config/nvim
+touch ~/.config/nvim/init.vim
+
+echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after" >> ~/.config/nvim/init.vim
+echo "let &packpath = &runtimepath" >> ~/.config/nvim/init.vim
+echo "source ~/.vimrc" >> ~/.config/nvim/init.vim
