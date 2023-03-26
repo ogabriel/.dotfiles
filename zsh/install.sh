b2f3b8f9c0f888262f71cfcc5459b42d760ec1e6
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2023-03-26 17:07:34 -0300

    update zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
 
 chsh -s $(which zsh)
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
