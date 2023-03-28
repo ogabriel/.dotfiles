2f6ec4dbfb6645e4de07becab4fbfdf230f46890
ogabriel <gabrieloliver8991@gmail.com>
2023-03-28 18:36:41 -0300

    update file

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,10 +1,21 @@
-#!/usr/bin/env zsh
+#!/bin/zsh
+
+rm .zshrc
 
 # install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# link files
+ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
+
 # install vim plugin
 git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
 
-# link files
-ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
+# install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+# install zsh-automatic
+git clone https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/plugins/zsh-completions
+
+# install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
