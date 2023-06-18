3d344b8974a9bdabb0ef15c7f4af64418fb68833
ogabriel <gabrieloliver8991@gmail.com>
2023-06-18 17:36:43 -0300

    add fzf file

---
+++ nvim_new/lua/plugins/fzf.lua
@@ -0,0 +1,11 @@
+return {
+	"junegunn/fzf.vim",
+	  dependencies = {
+	    'junegunn/fzf',
+	    build = function() vim.fn['fzf#install']() end,
+	  },
+	keys = {
+		{ '<C-f>', ':Ag<cr>' },
+		{ '<C-p>', ':Files<cr>' },
+	},
+}
