3e240e5cd0f94966e8ab80765e7fca660abc426f
ogabriel <gabrieloliver8991@gmail.com>
2023-06-18 16:07:24 -0300

    add init.lua

---
+++ nvim_new/init.lua
@@ -0,0 +1,46 @@
+--base lazy.nvim config
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+
+require("lazy").setup("plugins", {})
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+
+local map = vim.api.nvim_set_keymap
+
+options = { noremap = true }
+
+map( 'n', '<leader><leader>', ':NvimTreeToggle<cr>', options)
+map( 'n', '<leader>c', ':NvimTreeFindFileToggle<cr>', options)
