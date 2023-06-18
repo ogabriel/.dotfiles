1653f4750e08d51aa8cdc040183eeea04cf4046d
ogabriel <gabrieloliver8991@gmail.com>
2023-06-18 18:12:57 -0300

    updpate fzf cofnig

--- nvim_new/lua/plugins/fzf.lua
+++ nvim_new/lua/plugins/fzf.lua
@@ -1,9 +1,9 @@
 return {
 	"junegunn/fzf.vim",
-	  dependencies = {
-	    'junegunn/fzf',
-	    build = function() vim.fn['fzf#install']() end,
-	  },
+	dependencies = {
+		'junegunn/fzf',
+		build = function() vim.fn['fzf#install']() end,
+	},
 	keys = {
 		{ '<C-f>', ':Ag<cr>' },
 		{ '<C-p>', ':Files<cr>' },
