0d23984a1529590234bb6e652f78b7f81ff83ae2
ogabriel <gabrieloliver8991@gmail.com>
2023-06-28 08:37:54 -0300

    fix again

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p ~/.config/alacritty
-mv ~/.config/alacritty.yml ~/.config/alacritty.yml.bak
+mv ~/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml.bak
 
 ln -s ~/.dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
