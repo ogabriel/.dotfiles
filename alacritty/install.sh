9ce98a8d96c6f58c3cd52e572bcf6f977d7def9a
ogabriel <gabrieloliver8991@gmail.com>
2023-06-28 08:36:39 -0300

    fix alacritty script

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p ~/.config/alacritty
-cp ~/.config/alacritty.yml ~/.config/alacritty.yml.bak
+mv ~/.config/alacritty.yml ~/.config/alacritty.yml.bak
 
 ln -s ~/.dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
