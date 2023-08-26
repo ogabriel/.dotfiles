925d28320451066467adedbc2522403ed182226b
ogabriel <gabrieloliver8991@gmail.com>
2023-08-25 22:33:21 -0300

    add install nvim

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,23 +1,7 @@
 #!/bin/sh
 
-set -e
+if [ -d ~/.config/nvim ]; then
+    mv ~/.config/nvim ~/.config/nvim.bak
+fi
 
-cd /tmp
-
-FILENAME=nvim-linux64.deb
-
-wget -O /tmp/$FILENAME https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
-wget -O /tmp/$FILENAME https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb.sha256sum
-
-sha256sum /tmp/$FILENAME > /tmp/checksum
-sha256sum -c /tmp/checksum
-
-sudo dpkg -i /tmp/$FILENAME
-
-mkdir -p ~/.config
-mkdir -p ~/.config/nvim
-touch ~/.config/nvim/init.vim
-
-echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after" >> ~/.config/nvim/init.vim
-echo "let &packpath = &runtimepath" >> ~/.config/nvim/init.vim
-echo "source ~/.vimrc" >> ~/.config/nvim/init.vim
+ln -sf ~/.dotfiles/nvim/ ~/.config/nvim
