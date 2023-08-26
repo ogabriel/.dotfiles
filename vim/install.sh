cb5fa16774a5cd0b61abfcd89a10c72bc2874a2f
ogabriel <gabrieloliver8991@gmail.com>
2023-08-25 22:01:18 -0300

    update install script

--- vim/install.sh
+++ vim/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# read this file carefuly, then, run the command:
-cd
 
-# just adding a source to the files at HOME, to redirect to the dotfile configuration
-ln -sf ~/.dotfiles/vim/vimrc ~/.vimrc
-ln -sf ~/.dotfiles/vim/vimrc.plugins ~/.vimrc.plugins
+if [ -d ~/.vim ]; then
+    mv ~/.vim ~/.vim.bak
+fi
+
+ln -sf ~/.dotfiles/vim/ ~/.vim
