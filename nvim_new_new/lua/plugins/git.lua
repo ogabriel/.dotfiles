7f31b1e0ee06a9e60aef0526ac23aca152bc539a
ogabriel <gabrieloliver8991@gmail.com>
2023-08-27 13:13:27 -0300

    add git file

---
+++ nvim_new_new/lua/plugins/git.lua
@@ -0,0 +1,11 @@
+return {
+    'tpope/vim-fugitive',
+      keys = {
+        { '<leader>gs', ':Git<cr>' },
+        { '<leader>ga', ':Gwrite<cr>' },
+        { '<leader>gd', ':Git diff<cr>' },
+        { '<leader>gc', ':Git commit<cr>' },
+        { '<leader>gb', ':Git blame<cr>' },
+        { '<leader>gd', ':GDelete<cr>' },
+      }
+}
