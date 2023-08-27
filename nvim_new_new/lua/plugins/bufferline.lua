189b2d1815d6524431d2d7a608d67067fa18be97
ogabriel <gabrieloliver8991@gmail.com>
2023-08-27 19:13:04 -0300

    update bufferline

--- nvim_new_new/lua/plugins/bufferline.lua
+++ nvim_new_new/lua/plugins/bufferline.lua
@@ -1,14 +1,18 @@
 return {
 	'akinsho/bufferline.nvim',
 	version = "*",
+    event = { "BufEnter" },
     dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
-	config = function(plugin)
-		require("bufferline").setup {
-			options = {
-				separator_style = "slant"
-			}
-		}
-	end,
+    opts = {
+        options = {
+            -- stylua: ignore
+            close_command = function(n) require("mini.bufremove").delete(n, false) end,
+            -- stylua: ignore
+            right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+            diagnostics = "nvim_lsp",
+            always_show_bufferline = false,
+        },
+    },
     keys = {
         { 'q', ':BufferLineCyclePrev<cr>' },
         { 'w', ':BufferLineCycleNext<cr>' },
