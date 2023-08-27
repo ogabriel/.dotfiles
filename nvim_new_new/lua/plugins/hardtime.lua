7191a35436f6e49843d82013e25cf6478f658236
ogabriel <gabrieloliver8991@gmail.com>
2023-08-27 18:38:08 -0300

    add commented out hardtime plugin

---
+++ nvim_new_new/lua/plugins/hardtime.lua
@@ -0,0 +1,11 @@
+return {
+    -- messes up too many things
+   -- "m4xshen/hardtime.nvim",
+   -- dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+   -- opts = {
+   --      disabled_keys = {
+   --         ["<Up>"] = {},
+   --         ["<Space>"] = { "n", "x" },
+   --      },
+   --  },
+}
