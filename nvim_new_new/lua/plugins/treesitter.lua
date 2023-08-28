cce78882bf5441074c85703cff60f04066a964aa
ogabriel <gabrieloliver8991@gmail.com>
2023-08-28 20:15:28 -0300

    update treesitter

--- nvim_new_new/lua/plugins/treesitter.lua
+++ nvim_new_new/lua/plugins/treesitter.lua
@@ -1,24 +1,37 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function ()
-	local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-	configs.setup({
-	    ensure_installed = {
-		"c", "rust",
-		"lua", "vim", "vimdoc", "query",
-		"elixir", "heex", "eex",
-		"ruby",
-		"javascript", "html", "css",
-		"csv", "dockerfile"
-		},
-	    sync_install = false,
-	    highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false
-	    },
-	    indent = { enable = true },
-	})
-    end
+		configs.setup({
+			ensure_installed = {
+				"c",
+				"rust",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"erlang",
+				"elixir",
+				"heex",
+				"eex",
+				"ruby",
+				"javascript",
+				"html",
+				"css",
+				"csv",
+				"dockerfile",
+				"surface",
+			},
+			sync_install = false,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = {
+				enable = true,
+			},
+		})
+	end,
 }
