625c0fc701c56a532e5587674d01dd2480588cf5
ogabriel <gabrieloliver8991@gmail.com>
2023-08-28 20:25:03 -0300

    add flash

---
+++ nvim_new_new/lua/plugins/flash.lua
@@ -0,0 +1,63 @@
+return {
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	---@type Flash.Config
+	opts = {
+		multi_window = true,
+		modes = {
+			char = {
+				enabled = true,
+				jump_labels = true,
+				keys = { ";", "," },
+			},
+		},
+	},
+	keys = {
+		{
+			"f",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump({
+					search = {
+						mode = function(str)
+							return "\\<" .. str
+						end,
+					},
+				})
+			end,
+			desc = "Flash",
+		},
+		{
+			"t",
+			mode = { "n", "o", "x" },
+			function()
+				require("flash").treesitter()
+			end,
+			desc = "Flash Treesitter",
+		},
+		{
+			"r",
+			mode = "o",
+			function()
+				require("flash").remote()
+			end,
+			desc = "Remote Flash",
+		},
+		{
+			"R",
+			mode = { "o", "x" },
+			function()
+				require("flash").treesitter_search()
+			end,
+			desc = "Treesitter Search",
+		},
+		{
+			"<c-s>",
+			mode = { "c" },
+			function()
+				require("flash").toggle()
+			end,
+			desc = "Toggle Flash Search",
+		},
+	},
+}
