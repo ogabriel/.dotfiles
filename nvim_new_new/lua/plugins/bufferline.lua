adee1279d47e7510372acfc30b4f375583bff86a
ogabriel <gabrieloliver8991@gmail.com>
2023-08-28 20:23:54 -0300

    update bufferline

--- nvim_new_new/lua/plugins/bufferline.lua
+++ nvim_new_new/lua/plugins/bufferline.lua
@@ -1,20 +1,24 @@
 return {
-	'akinsho/bufferline.nvim',
+	"akinsho/bufferline.nvim",
 	version = "*",
-    event = { "BufEnter" },
-    dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
-    opts = {
-        options = {
-            -- stylua: ignore
-            close_command = function(n) require("mini.bufremove").delete(n, false) end,
-            -- stylua: ignore
-            right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
-            diagnostics = "nvim_lsp",
-            always_show_bufferline = false,
-        },
-    },
-    keys = {
-        { 'q', ':BufferLineCyclePrev<cr>' },
-        { 'w', ':BufferLineCycleNext<cr>' },
-    }
+	event = { "BufEnter" },
+	dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
+	opts = {
+		options = {
+			close_command = function(n)
+				require("mini.bufremove").delete(n, false)
+			end,
+			right_mouse_command = function(n)
+				require("mini.bufremove").delete(n, false)
+			end,
+			diagnostics = "nvim_lsp",
+			always_show_bufferline = false,
+		},
+	},
+	keys = {
+		{ "q", ":BufferLineCyclePrev<cr>" },
+		{ "w", ":BufferLineCycleNext<cr>" },
+		{ "(", ":BufferLineGoToBuffer 1<cr>" },
+		{ ")", ":BufferLineGoToBuffer 0<cr>" },
+	},
 }
