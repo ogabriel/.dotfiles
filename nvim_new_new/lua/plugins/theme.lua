acdc40e8bab52d4144048895842cf5449e84e7f5
ogabriel <gabrieloliver8991@gmail.com>
2023-08-28 20:24:42 -0300

    update theme

--- nvim_new_new/lua/plugins/theme.lua
+++ nvim_new_new/lua/plugins/theme.lua
@@ -1,11 +1,13 @@
 return {
 	--plugin that is supported by treesitter
 	"navarasu/onedark.nvim",
-	config = function(plugin)
+	config = function(_)
 		vim.opt.termguicolors = true
-		require('onedark').setup {
-    		style = 'darker'
-		}
-		require('onedark').load()
+
+		require("onedark").setup({
+			style = "darker",
+		})
+
+		require("onedark").load()
 	end,
 }
