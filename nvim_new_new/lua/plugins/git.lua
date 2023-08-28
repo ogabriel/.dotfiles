e7f632542914995140855a54bd13520db4432282
ogabriel <gabrieloliver8991@gmail.com>
2023-08-28 20:19:16 -0300

    update git

--- nvim_new_new/lua/plugins/git.lua
+++ nvim_new_new/lua/plugins/git.lua
@@ -1,16 +1,16 @@
 return {
-    'tpope/vim-fugitive',
-      keys = {
-        { '<leader>gs', ':Git<cr>' },
-        { '<leader>ga', ':Gwrite<cr>' },
-        { '<leader>gd', ':Git diff<cr>' },
-        { '<leader>gc', ':Git commit<cr>' },
-        { '<leader>gb', ':Git blame<cr>' },
-        { '<leader>gd', ':GDelete<cr>' },
-        { '<leader>gu', ':Git push<cr>' },
-        { '<leader>gp', ':Git pull<cr>' },
-        --its not working
-        { '<leader>gm', ':GMove<cr>' },
-        { '<leader>gr', ':GReneme<cr>' },
-      }
+	"tpope/vim-fugitive",
+	keys = {
+		{ "<leader>gs", ":Git<cr>" },
+		{ "<leader>ga", ":Gwrite<cr>" },
+		{ "<leader>gd", ":Git diff<cr>" },
+		{ "<leader>gc", ":Git commit<cr>" },
+		{ "<leader>gb", ":Git blame<cr>" },
+		{ "<leader>gd", ":GDelete<cr>" },
+		{ "<leader>gu", ":Git push<cr>" },
+		{ "<leader>gp", ":Git pull<cr>" },
+		--its not working
+		{ "<leader>gm", ":GMove %<cr>" },
+		{ "<leader>gr", ":GRename %<cr>" },
+	},
 }
