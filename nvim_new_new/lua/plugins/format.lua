070ac0137890065993dcab82b12677dd2858b22d
ogabriel <gabrieloliver8991@gmail.com>
2023-08-28 20:27:06 -0300

    add formatter

---
+++ nvim_new_new/lua/plugins/format.lua
@@ -0,0 +1,40 @@
+return {
+	"mhartington/formatter.nvim",
+	event = { "BufWritePost" },
+	cmd = { "Format", "FormatWrite" },
+	config = function(_)
+		local group = vim.api.nvim_create_augroup("FormatAutogroup", { clear = true })
+
+		vim.api.nvim_create_autocmd("BufWritePost", {
+			command = "FormatWrite",
+			group = group,
+		})
+
+		-- vim.api.nvim_create_autocmd("BufWritePre", {
+		-- 	callback = function()
+		-- 		vim.lsp.buf.format()
+		-- 	end,
+		-- 	group = group,
+		-- })
+
+		require("formatter").setup({
+			logging = true,
+			log_level = vim.log.levels.WARN,
+			filetype = {
+				elixir = { require("formatter.filetypes.elixir").mixformat },
+				lua = { require("formatter.filetypes.lua").stylua },
+				html = { require("formatter.filetypes.html").prettier },
+				-- eelixir = {
+				-- 	require("formatter.util").withl(require("formatter.defaults").prettier, "html"),
+				-- },
+				["*"] = {
+					require("formatter.filetypes.any").remove_trailing_whitespace,
+				},
+			},
+		})
+	end,
+	keys = {
+		{ "<leader>fw", ":Format<CR>", silent = true },
+		{ "<leader>fW", ":FormatWrite<CR>", silent = true },
+	},
+}
