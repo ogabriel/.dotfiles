a8c2a83469c514712f3ca8e48b7c935d668c733d
ogabriel <gabrieloliver8991@gmail.com>
2023-08-28 20:23:00 -0300

    add code runner

--- nvim_new_new/lua/plugins/compiler.lua
+++ nvim_new_new/lua/plugins/compiler.lua
@@ -1,24 +1,25 @@
 return {
-    "CRAG666/code_runner.nvim",
-    dependencies = 'nvim-lua/plenary.nvim',
-    config = function (_)
-        require('code_runner').setup({
-            filetype = {
-                java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
-                python = "python3 -u",
-                typescript = "deno run",
-                rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt",
-                c = "cd $dir && gcc $fileName -lm && $dir/$fileNameWithoutExt",
-                cpp = "cd $dir && g++ $fileName && ./a.out",
-                perl = "perl -w",
-            },
-            focus = true,
-            mode = 'term',
-            sh = "zsh",
-        })
-    end,
-    keys = {
-        { '<leader>rc', ':RunCode<cr>' },
-        { '<leader>rp', ':RunProject<cr>' },
-    },
+	"CRAG666/code_runner.nvim",
+	config = function(_)
+		require("code_runner").setup({
+			filetype = {
+				java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
+				elixir = "cd $dir && mix compile",
+				python = "python3 -u",
+				typescript = "deno run",
+				rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt",
+				c = "cd $dir && gcc $fileName -lm && $dir/$fileNameWithoutExt",
+				cpp = "cd $dir && g++ $fileName && ./a.out",
+				perl = "perl -w",
+			},
+			focus = true,
+			mode = "term",
+			sh = "zsh",
+		})
+	end,
+	cmd = { "RunCode", "RunProject" },
+	keys = {
+		{ "<leader>rc", ":RunCode<cr>" },
+		{ "<leader>rp", ":RunProject<cr>" },
+	},
 }
