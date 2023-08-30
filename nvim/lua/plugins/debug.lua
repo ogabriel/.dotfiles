17f8b84132faa7d20fb5cac5b11fe44bb8d42b89
ogabriel <gabrieloliver8991@gmail.com>
2023-08-29 22:26:28 -0300

    add debug file

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -1,23 +1,51 @@
---WIP
 return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
-		{ "williamboman/mason.nvim" }, -- Optional
+		{
+			"jay-babu/mason-nvim-dap.nvim",
+			dependencies = { "nvim-dap" },
+			cmd = { "DapInstall", "DapUninstall" },
+			opts = { handlers = {} },
+		},
+		{
+			"rcarriga/nvim-dap-ui",
+			opts = { floating = { border = "rounded" } },
+			config = function(_, opts)
+				local dap, dapui = require("dap"), require("dapui")
+
+				dap.listeners.after.event_initialized["dapui_config"] = function()
+					dapui.open()
+				end
+				dap.listeners.before.event_terminated["dapui_config"] = function()
+					dapui.close()
+				end
+				dap.listeners.before.event_exited["dapui_config"] = function()
+					dapui.close()
+				end
+
+				dapui.setup(opts)
+			end,
+		},
+		{
+			"rcarriga/cmp-dap",
+			dependencies = { "nvim-cmp" },
+			config = function()
+				require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+					sources = {
+						{ name = "dap" },
+					},
+				})
+			end,
+		},
 	},
-	keys = {
-		{ "<leader>bt", "<cmd>lua require('dap').toggle_breakpoint()<cr>" },
-		{ "<leader>br", "<cmd>lua require('dap').run()<cr>" },
-		{ "<leader>bc", "<cmd>lua require('dap').continue()<cr>" },
-		{ "<leader>bs", "<cmd>lua require('dap').step_into()<cr>" },
-		{ "<leader>bo", "<cmd>lua require('dap').repl.open()<cr>" },
-	},
-	config = function(_)
+	config = function()
 		local dap = require("dap")
 
+		--WARN: some elixir projects just get errors, like memory leaks and you cant really use it
 		dap.configurations.elixir = {
 			{
 				type = "mix_task",
-				name = "mix test",
+				name = "start tests",
 				task = "test",
 				taskArgs = { "--trace" },
 				request = "launch",
@@ -32,19 +60,25 @@ return {
 			},
 			{
 				type = "mix_task",
-				name = "phx.server",
-				request = "launch",
+				name = "start phoenix",
 				task = "phx.server",
-				projectDir = ".",
+				request = "launch",
+				projectDir = "${workspaceFolder}",
 			},
 		}
 
-		local command = os.getenv("HOME") .. "/.local/share/nvim/mason" .. "/bin/elixir-ls-debugger"
-
 		dap.adapters.mix_task = {
 			type = "executable",
-			command = command,
+			command = os.getenv("HOME") .. "/.local/share/nvim/mason/packages/elixir-ls/debugger.sh",
 			args = {},
 		}
 	end,
+	keys = {
+		{ "<leader>bt", "<cmd>lua require('dap').toggle_breakpoint()<cr>" },
+		{ "<leader>br", "<cmd>lua require('dap').run()<cr>" },
+		{ "<leader>bc", "<cmd>lua require('dap').continue()<cr>" },
+		{ "<leader>bs", "<cmd>lua require('dap').step_into()<cr>" },
+		{ "<leader>bo", "<cmd>lua require('dap').repl.open()<cr>" },
+		{ "<leader>but", "<cmd>lua require('dapui').toggle()<cr>" },
+	},
 }
