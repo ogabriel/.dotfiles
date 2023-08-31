06f1e79f1a4a6bdb8d68b60a20e521e08b3f5b73
ogabriel <gabrieloliver8991@gmail.com>
2023-08-31 07:20:06 -0300

    update lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -10,18 +10,27 @@ return {
 		-- Autocompletion
 		{ "hrsh7th/nvim-cmp" }, -- Required
 		{ "hrsh7th/cmp-nvim-lsp" }, -- Required
-		{ "L3MON4D3/LuaSnip" }, -- Required
+		{
+			"L3MON4D3/LuaSnip",
+			dependencies = { "rafamadriz/friendly-snippets" },
+			version = "2.*",
+			build = "make install_jsregexp",
+			config = function(_)
+				require("luasnip.loaders.from_vscode").lazy_load()
+				require("luasnip.loaders.from_snipmate").lazy_load()
+			end,
+		},
 	},
 	config = function()
 		local lsp = require("lsp-zero").preset({})
 
-		lsp.on_attach(function(client, bufnr)
+		lsp.on_attach(function(_, bufnr)
 			-- see :help lsp-zero-keybindings
 			-- to learn the available actions
 			lsp.default_keymaps({ buffer = bufnr })
 
+			--lsp format, i dont like it too much, but i will let it stay here
 			-- local group = vim.api.nvim_create_augroup("FormatAutogroup", { clear = true })
-			--
 			-- if client.server_capabilities.documentFormattingProvider then
 			-- 	vim.api.nvim_create_autocmd("BufWritePre", {
 			-- 		callback = function()
