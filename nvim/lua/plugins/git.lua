1e1cf8fbab5adeac18347dc1ce98a82c91ce4c89
ogabriel <gabrieloliver8991@gmail.com>
2023-08-31 18:29:51 -0300

    update git

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,19 +1,32 @@
 return {
 	{
+		"airblade/vim-gitgutter",
+		event = { "BufReadPost", "BufNewFile" },
+		filetype_exclude = {
+			"help",
+			"alpha",
+			"dashboard",
+			"neo-tree",
+			"Trouble",
+			"lazy",
+			"mason",
+			"notify",
+			"toggleterm",
+			"lazyterm",
+		},
+	},
+	{
 		"tpope/vim-fugitive",
 		keys = {
 			{ "<leader>gs", ":Git<cr>" },
 			{ "<leader>ga", ":Gwrite<cr>" },
 			{ "<leader>gd", ":Git diff<cr>" },
 			{ "<leader>gc", ":Git commit<cr>" },
-			{ "<leader>gf", ":Gwrite <bar> :Git commit --fixup HEAD -n <cr>" },
+			{ "<leader>gf", ":Gwrite <cr><bar> :Git commit --fixup HEAD -n <cr>" },
 			{ "<leader>gb", ":Git blame<cr>" },
 			{ "<leader>gd", ":GDelete<cr>" },
 			{ "<leader>gu", ":Git push<cr>" },
 			{ "<leader>gp", ":Git pull<cr><cr>" },
-			--its not working
-			-- { "<leader>gm", ":GMove %<cr>" },
-			-- { "<leader>gr", ":GRename %<cr>" },
 		},
 	},
 	{
@@ -26,10 +39,22 @@ return {
 			{
 				"<leader>grb",
 				'<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+				{ silent = true },
+			},
+			{
+				"<leader>grb",
+				mode = { "v" },
+				'<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
 			},
 			{
 				"<leader>grc",
 				'<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".copy_to_clipboard})<cr>',
+			},
+			{
+				"<leader>grc",
+				mode = { "v" },
+				'<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".copy_to_clipboard})<cr>',
+				{ silent = true },
 			},
 		},
 	},
