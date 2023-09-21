0e55c30d56f15ba20b3c3751c10f0e05f87ee3bb
ogabriel <gabrieloliver8991@gmail.com>
2023-09-20 21:11:14 -0300

    update compiler

--- nvim/lua/plugins/compiler.lua
+++ nvim/lua/plugins/compiler.lua
@@ -1,3 +1,11 @@
+-- vim.keymap.set('n', '<leader>r', ':RunCode<CR>', { noremap = true, silent = false })
+-- vim.keymap.set('n', '<leader>rf', ':RunFile<CR>', { noremap = true, silent = false })
+-- vim.keymap.set('n', '<leader>rft', ':RunFile tab<CR>', { noremap = true, silent = false })
+-- vim.keymap.set('n', '<leader>rp', ':RunProject<CR>', { noremap = true, silent = false })
+-- vim.keymap.set('n', '<leader>rc', ':RunClose<CR>', { noremap = true, silent = false })
+-- vim.keymap.set('n', '<leader>crf', ':CRFiletype<CR>', { noremap = true, silent = false })
+-- vim.keymap.set('n', '<leader>crp', ':CRProjects<CR>', { noremap = true, silent = false })
+
 return {
 	"CRAG666/code_runner.nvim",
 	config = function(_)
@@ -10,16 +18,20 @@ return {
 				rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt",
 				c = "cd $dir && gcc $fileName -lm && $dir/$fileNameWithoutExt",
 				cpp = "cd $dir && g++ $fileName && ./a.out",
+				go = "cd $dir && go run $fileName",
 				perl = "perl -w",
 			},
-			focus = true,
+			focus = false,
 			mode = "term",
 			sh = "zsh",
 		})
 	end,
 	cmd = { "RunCode", "RunProject" },
 	keys = {
-		{ "<leader>rc", ":RunCode<cr>" },
+		{ "<leader>r", ":RunCode<cr>" },
+		{ "<leader>rf", ":RunFile<cr>" },
+		{ "<leader>rft", ":RunFile tab<cr>" },
 		{ "<leader>rp", ":RunProject<cr>" },
+		{ "<leader>rc", ":RunClose<cr>" },
 	},
 }
