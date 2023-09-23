739ce8f07461db9392c9cd17fee628c4fb83e776
ogabriel <gabrieloliver8991@gmail.com>
2023-09-23 09:19:12 -0300

    update dap

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,11 +1,9 @@
+-- debug functionality
 return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
 		{
 			"jay-babu/mason-nvim-dap.nvim",
-			dependencies = { "nvim-dap" },
-			cmd = { "DapInstall", "DapUninstall" },
-			opts = { handlers = {} },
 		},
 		{
 			"rcarriga/nvim-dap-ui",
@@ -71,6 +69,40 @@ return {
 			type = "executable",
 			command = os.getenv("HOME") .. "/.local/share/nvim/mason/packages/elixir-ls/debugger.sh",
 			args = {},
+		}
+
+		dap.adapters.delve = {
+			type = "server",
+			port = "${port}",
+			executable = {
+				command = "dlv",
+				args = { "dap", "-l", "127.0.0.1:${port}" },
+			},
+		}
+
+		-- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+		dap.configurations.go = {
+			{
+				type = "delve",
+				name = "Debug",
+				request = "launch",
+				program = "${file}",
+			},
+			{
+				type = "delve",
+				name = "Debug test", -- configuration for debugging test files
+				request = "launch",
+				mode = "test",
+				program = "${file}",
+			},
+			-- works with go.mod packages and sub packages
+			{
+				type = "delve",
+				name = "Debug test (go.mod)",
+				request = "launch",
+				mode = "test",
+				program = "./${relativeFileDirname}",
+			},
 		}
 	end,
 	keys = {
