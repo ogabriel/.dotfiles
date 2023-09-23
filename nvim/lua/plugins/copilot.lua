1e3ca039db67f2d66160b0f4d4e4d766476861f8
ogabriel <gabrieloliver8991@gmail.com>
2023-09-23 07:19:54 -0300

    update copilot configs

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,3 +1,23 @@
 return {
 	"github/copilot.vim",
+	config = function()
+		vim.cmd('imap <silent><script><expr> <C-CR> copilot#Accept("\\<CR>")')
+
+		vim.g.copilot_no_tab_map = true
+
+		vim.g.copilot_filetypes = {
+			["*"] = false,
+			["javascript"] = true,
+			["typescript"] = true,
+			["lua"] = false,
+			["rust"] = true,
+			["c"] = true,
+			["c#"] = true,
+			["c++"] = true,
+			-- ["go"] = true,
+			["python"] = true,
+			["ruby"] = true,
+			["elixir"] = true,
+		}
+	end,
 }
