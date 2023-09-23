acde223e2c64bdb6030726bcb4be378425385bfb
ogabriel <gabrieloliver8991@gmail.com>
2023-09-23 10:36:59 -0300

    update

--- nvim/lua/plugins/mason-nvim-dap.lua
+++ nvim/lua/plugins/mason-nvim-dap.lua
@@ -1,7 +1,9 @@
 return {
 	"jay-babu/mason-nvim-dap.nvim",
+	event = "VeryLazy",
 	dependecies = {
 		"williamboman/mason.nvim",
+		"mfussenegger/nvim-dap",
 	},
 	config = function(opts)
 		require("mason").setup()
