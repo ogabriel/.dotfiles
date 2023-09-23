1e40f04fa802994201002dc5702fae28be26ff4d
ogabriel <gabrieloliver8991@gmail.com>
2023-09-23 07:20:33 -0300

    add commented other

---
+++ nvim/lua/plugins/other.lua
@@ -0,0 +1,12 @@
+return {
+	-- "rgroli/other.nvim",
+	--		event = "VeryLazy",
+	-- config = function(_)
+	-- 	require("other-nvim").setup({
+	-- 		mappings = {
+	-- 			"rails",
+	-- 			"golang",
+	-- 		},
+	-- 	})
+	-- end,
+}
