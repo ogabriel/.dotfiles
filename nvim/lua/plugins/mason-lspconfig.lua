0fa22f6870cbe96248fadc208f1d77d53d7dda0e
ogabriel <gabrieloliver8991@gmail.com>
2023-09-23 09:16:47 -0300

    add mason-lspconfig

---
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,15 @@
+return {
+	"williamboman/mason-lspconfig.nvim",
+	config = function(opts)
+		require("mason").setup()
+		require("mason-lspconfig").setup(opts)
+	end,
+	opts = {
+		ensure_installed = {
+			"elixirls",
+			"gopls",
+			"lua_ls",
+			"rust_analyzer",
+		},
+	},
+}
