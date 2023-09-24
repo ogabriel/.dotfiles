4cf898e5b4b5e71aa18cee522adc8b7e51be9725
ogabriel <gabrieloliver8991@gmail.com>
2023-09-23 21:04:10 -0300

    update copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -6,18 +6,7 @@ return {
 		vim.g.copilot_no_tab_map = true
 
 		vim.g.copilot_filetypes = {
-			["*"] = false,
-			["javascript"] = true,
-			["typescript"] = true,
-			["lua"] = false,
-			["rust"] = true,
-			["c"] = true,
-			["c#"] = true,
-			["c++"] = true,
-			-- ["go"] = true,
-			["python"] = true,
-			["ruby"] = true,
-			["elixir"] = true,
+			["go"] = false,
 		}
 	end,
 }
