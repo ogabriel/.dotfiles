cd6104c0dc0befe7df0cbe9b38f4e915f8c28cb5
ogabriel <gabrieloliver8991@gmail.com>
2023-09-25 10:52:44 -0300

    fix ruby formatter

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -41,12 +41,18 @@ return {
 						return {
 							exe = "rubocop",
 							args = {
+								"--fix-layout",
 								"--stdin",
 								util.escape_path(util.get_current_buffer_file_name()),
 								"--format",
 								"files",
 							},
 							stdin = true,
+							transform = function(text)
+								table.remove(text, 1)
+								table.remove(text, 1)
+								return text
+							end,
 						}
 					end,
 				},
