282720550af2cb7a2a5230073f3cd597b10985e6
ogabriel <gabrieloliver8991@gmail.com>
2023-09-28 17:09:38 -0300

    disable fzf

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,29 +1,28 @@
 return {
-	"ibhagwan/fzf-lua",
-	enabled = false,
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-		{ "junegunn/fzf", build = "./install --bin" },
-	},
-	config = function()
-		-- calling `setup` is optional for customization
-		require("fzf-lua").setup({})
-	end,
-	keys = {
-		{
-			"<c-p>",
-			mode = { "n", "v" },
-			":FzfLua files<CR>",
-		},
-		{
-			"<c-f>",
-			mode = { "n" },
-			":FzfLua grep_project<CR>",
-		},
-		{
-			"<c-f>",
-			mode = { "v" },
-			":FzfLua grep_visual<cr>",
-		},
-	},
+	-- "ibhagwan/fzf-lua",
+	-- dependencies = {
+	-- 	"nvim-tree/nvim-web-devicons",
+	-- 	{ "junegunn/fzf", build = "./install --bin" },
+	-- },
+	-- config = function()
+	-- 	-- calling `setup` is optional for customization
+	-- 	require("fzf-lua").setup({})
+	-- end,
+	-- keys = {
+	-- 	{
+	-- 		"<c-p>",
+	-- 		mode = { "n", "v" },
+	-- 		":FzfLua files<CR>",
+	-- 	},
+	-- 	{
+	-- 		"<c-f>",
+	-- 		mode = { "n" },
+	-- 		":FzfLua grep_project<CR>",
+	-- 	},
+	-- 	{
+	-- 		"<c-f>",
+	-- 		mode = { "v" },
+	-- 		":FzfLua grep_visual<cr>",
+	-- 	},
+	-- },
 }
