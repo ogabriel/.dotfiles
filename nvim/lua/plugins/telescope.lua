0cda9333d813d85e45913c8e880419802f44dcf1
ogabriel <gabrieloliver8991@gmail.com>
2023-09-27 21:43:16 -0300

    add many some telescope functions

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -16,6 +16,7 @@ return {
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
+		local putils = require("telescope.previewers.utils")
 
 		telescope.setup({
 			defaults = {
@@ -34,13 +35,38 @@ return {
 					},
 				},
 				preview = {
-					treesitter = {
-						disable = {
-							"javascript",
-							"css",
-							"scss",
-						},
-					},
+					timeout = 50,
+					filesize_limit = 1,
+					treesitter = false,
+					-- 1) Do not show previewer for certain files
+					filetype_hook = function(filepath, bufnr, opts)
+						-- you could analogously check opts.ft for filetypes
+						local excluded = vim.tbl_filter(function(ending)
+							return filepath:match(ending)
+						end, {
+							".*%.js",
+							".*%.css",
+							".*%.csv",
+							".*%.toml",
+						})
+						if not vim.tbl_isempty(excluded) then
+							putils.set_preview_message(
+								bufnr,
+								opts.winid,
+								string.format("I don't like %s files!", excluded[1]:sub(5, -1))
+							)
+							return false
+						end
+						return true
+					end,
+					-- 2) Truncate lines to preview window for too large files
+					filesize_hook = function(filepath, bufnr, opts)
+						local path = require("plenary.path"):new(filepath)
+						-- opts exposes winid
+						local height = vim.api.nvim_win_get_height(opts.winid)
+						local lines = vim.split(path:head(height), "[\r]?\n")
+						vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+					end,
 				},
 			},
 			extensions = {
@@ -73,7 +99,7 @@ return {
 			":Telescope grep_string()<cr>",
 		},
 		{
-			"<leader>gd",
+			"<leader>gr",
 			mode = { "n", "v" },
 			":Telescope lsp_references<CR>",
 		},
