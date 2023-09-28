9c0eb8f17fec3858ee43bc9e8661316397f8aecc
ogabriel <gabrieloliver8991@gmail.com>
2023-09-28 10:57:57 -0300

    update oil configs

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,8 +1,21 @@
 return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
-	opts = {},
-	config = function(_)
-		require("oil").setup()
+	config = function(opts)
+		require("oil").setup(opts)
 	end,
+	opts = {
+		view_options = {
+			-- Show files and directories that start with "."
+			show_hidden = true,
+			-- This function defines what is considered a "hidden" file
+			is_hidden_file = function(name, bufnr)
+				return vim.startswith(name, ".")
+			end,
+			-- This function defines what will never be shown, even when `show_hidden` is set
+			is_always_hidden = function(name, bufnr)
+				return false
+			end,
+		},
+	},
 }
