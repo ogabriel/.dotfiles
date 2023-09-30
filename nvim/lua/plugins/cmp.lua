52b0c223ff32cee2613f2035eb26504c4cda645b
ogabriel <gabrieloliver8991@gmail.com>
2023-09-30 13:21:34 -0300

    change cmp tab behavior

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -30,11 +30,8 @@ return {
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				["<S-CR>"] = cmp.mapping.confirm({
-					behavior = cmp.ConfirmBehavior.Replace,
-					select = true,
-				}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				["<Tab>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }), --replace
+				["<S-Tab>"] = cmp.mapping.confirm({ select = true }), --only insert
 			}),
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
