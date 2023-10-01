f89306c42dca1f22428c451ba3e28c8c3ea64306
ogabriel <gabrieloliver8991@gmail.com>
2023-10-01 15:08:49 -0300

    remove cr confirm

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -54,9 +54,6 @@ return {
 
 		cmp.setup({
 			mapping = {
-				-- `Enter` key to confirm completion
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
-
 				-- Ctrl+Space to trigger completion menu
 				["<C-Space>"] = cmp.mapping.complete(),
 
