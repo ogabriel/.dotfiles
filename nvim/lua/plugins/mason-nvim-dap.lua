4dea930be353150ce2692844f4b4cc770c19a43d
ogabriel <gabrieloliver8991@gmail.com>
2023-10-01 12:10:42 -0300

    update mason-nvim-dap

--- nvim/lua/plugins/mason-nvim-dap.lua
+++ nvim/lua/plugins/mason-nvim-dap.lua
@@ -1,24 +1,19 @@
 return {
 	"jay-babu/mason-nvim-dap.nvim",
-	event = "VeryLazy",
 	dependecies = {
 		"williamboman/mason.nvim",
 		"mfussenegger/nvim-dap",
 	},
-	config = function(opts)
+	config = function()
 		require("mason").setup()
-		require("mason-nvim-dap").setup(opts)
+		require("mason-nvim-dap").setup({
+			ensure_installed = {
+				"elixir",
+				"delve",
+			},
+			automatic_installation = true,
+            --only use mason-nvim-dap to install things
+			handlers = nil,
+		})
 	end,
-	opts = {
-		ensure_installed = {
-			"elixir",
-			"delve",
-		},
-		automatic_installation = true,
-		handlers = {
-			function(config)
-				require("mason-nvim-dap").default_setup(config)
-			end,
-		},
-	},
 }
