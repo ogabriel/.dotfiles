f9c5755b8dfcaa702642873bbe592ba45abcd760
ogabriel <gabrieloliver8991@gmail.com>
2023-10-04 10:27:10 -0300

    update mini.bufremove

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,7 +1,20 @@
-return { 
-    'echasnovski/mini.bufremove', version = '*',
-    keys = {
-        { "<leader>x", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
-        { "<leader>X", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
-    },
+return {
+	"echasnovski/mini.bufremove",
+	version = "*",
+	keys = {
+		{
+			"<leader>X",
+			function()
+				require("mini.bufremove").delete(0, false)
+			end,
+			desc = "Delete Buffer",
+		},
+		{
+			"<leader>x",
+			function()
+				require("mini.bufremove").delete(0, true)
+			end,
+			desc = "Delete Buffer (Force)",
+		},
+	},
 }
