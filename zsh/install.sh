436d83747aaf5ceaba356682003acf71ecc95559
ogabriel <gabrieloliver8991@gmail.com>
2023-10-15 20:37:21 -0300

    feat: improve zsh install script

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-rm .zshrc
+if [ -f /etc/arch-release ]; then
+    sudo pacman -S --noconfirm --needed git zsh curl
+elif [ -f /etc/debian_version ]; then
+    sudo apt install -y git zsh curl
+fi
 
 # install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# link files
 ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
 ln -sf ~/.dotfiles/zsh/p10k.zsh ~/.p10k.zsh
