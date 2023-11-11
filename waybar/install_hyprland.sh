a39b89f65300ae09464908b53623cc0b6f0dcc44
ogabriel <gabrieloliver8991@gmail.com>
2023-11-11 16:21:10 -0300

    fix: install_hyprland

--- waybar/install_hyprland.sh
+++ waybar/install_hyprland.sh
@@ -6,6 +6,6 @@ fi
 
 mkdir -p ~/.config/waybar
 
-ln -sf ~/.dotfiles/waybar/hyprland_config /.config/waybar/config
-ln -sf ~/.dotfiles/waybar/style.css /.config/waybar/style.css
-ln -sf ~/.dotfiles/waybar/modules/ /.config/waybar/modules
+ln -sf ~/.dotfiles/waybar/hyprland_config ~/.config/waybar/config
+ln -sf ~/.dotfiles/waybar/style.css ~/.config/waybar/style.css
+ln -sf ~/.dotfiles/waybar/modules/ ~/.config/waybar/modules
