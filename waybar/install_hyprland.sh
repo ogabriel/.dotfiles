89040b4f94334793d77fb239e5b051d444fcc042
ogabriel <gabrieloliver8991@gmail.com>
2023-11-11 07:19:41 -0300

    feat: waybar hyprland script

---
+++ waybar/install_hyprland.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -d ~/.config/waybar ]; then
+    mv ~/.config/waybar ~/.config/waybar.bak
+fi
+
+mkdir -p ~/.config/waybar
+
+ln -sf ~/.dotfiles/waybar/hyprland_config /.config/waybar/config
+ln -sf ~/.dotfiles/waybar/style.css /.config/waybar/style.css
+ln -sf ~/.dotfiles/waybar/modules/ /.config/waybar/modules
