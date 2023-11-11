708781585ed6c86bddffc06f360fd07397dc4d64
ogabriel <gabrieloliver8991@gmail.com>
2023-11-10 21:29:08 -0300

    feat: add endwise to autopairs

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,26 +1,16 @@
--- https://github.com/windwp/nvim-autopairs#treesitter
 return {
 	"windwp/nvim-autopairs",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+	},
 	event = "InsertEnter",
 	config = function()
 		local npairs = require("nvim-autopairs")
-		local Rule = require("nvim-autopairs.rule")
 
-		npairs.setup({
-			check_ts = true,
-			ts_config = {
-				lua = { "string" }, -- it will not add a pair on that treesitter node
-				javascript = { "template_string" },
-				java = false, -- don't check treesitter on java
-			},
-		})
+		npairs.setup({})
 
-		local ts_conds = require("nvim-autopairs.ts-conds")
-
-		-- press % => %% only while inside a comment or string
-		npairs.add_rules({
-			Rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node({ "string", "comment" })),
-			Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node({ "function" })),
-		})
+		npairs.add_rules(require("nvim-autopairs.rules.endwise-elixir"))
+		npairs.add_rules(require("nvim-autopairs.rules.endwise-lua"))
+		npairs.add_rules(require("nvim-autopairs.rules.endwise-ruby"))
 	end,
 }
