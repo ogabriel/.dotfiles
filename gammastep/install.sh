577fe1453154505edf60346d7cd2106c1d54cb88
ogabriel <gabrieloliver8991@gmail.com>
2023-11-12 14:49:41 -0300

    feat: update gammastep

--- gammastep/install.sh
+++ gammastep/install.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+if [ -d ~/.config/gammastep ]; then
+    mv ~/.config/gammastep ~/.config/gammastep.bk
+fi
+
 mkdir -p ~/.config/gammastep
-ln -sf config.ini ~/.config/gammastep/config.ini
+
+ln -sf ~/.dotfiles/gammastep/config.ini ~/.config/gammastep/config.ini
