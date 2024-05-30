c4b34ae18028a4573eb28648693f5b3fabe195de
ogabriel <gabrieloliver8991@gmail.com>
2024-05-30 15:57:51 -0300

    update zsh to new version

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,8 +6,5 @@ elif [ -f /etc/debian_version ]; then
     sudo apt install -y git zsh curl
 fi
 
-# install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
-ln -sf ~/.dotfiles/zsh/p10k.zsh ~/.p10k.zsh
+ln -sf ~/.dotfiles/zsh/zimrc ~/.zimrc
--- zsh/zim_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ln -sf ~/.dotfiles/zsh/zshrc_zim ~/.zshrc
-ln -sf ~/.dotfiles/zsh/zimrc ~/.zimrc
