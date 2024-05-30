6f424baa34d25feb58ce15c751f6af950707aaec
ogabriel <gabrieloliver8991@gmail.com>
2024-05-30 16:02:52 -0300

    update tmux to new version

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-rm .tmux.conf.bak
-mr .tmux.conf.local.bak
-mv ~/.tmux.conf ~/.tmux.conf.bak
-mv ~/.tmux.conf.local ~/.tmux.conf.local.bak
+if [ -f /etc/arch-release ]; then
+    if ! pacman -Qs tmux >/dev/null || ! pacman -Qs zsh >/dev/null || ! pacman -Qs git >/dev/null; then
+        sudo pacman -S --needed --noconfirm tmux zsh git
+    fi
+fi
 
+mv ~/.tmux.conf ~/.tmux.conf.bak
 ln -sf ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -sf ~/.dotfiles/tmux/tmux.conf.local ~/.tmux.conf.local
--- tmux/new_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -f /etc/arch-release ]; then
-    if ! pacman -Qs tmux >/dev/null || ! pacman -Qs zsh >/dev/null || ! pacman -Qs git >/dev/null; then
-        sudo pacman -S --needed --noconfirm tmux zsh git
-    fi
-fi
-
-ln -sf ~/.dotfiles/tmux/tmux.new.conf ~/.tmux.conf
