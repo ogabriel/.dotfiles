d7d2dcb113af6d9c12f513f52696fe1936855eee
ogabriel <gabrieloliver8991@gmail.com>
2024-05-31 16:03:35 -0300

    update zsh installl script

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ -f /etc/arch-release ]; then
-    sudo pacman -S --noconfirm --needed git zsh curl
+    sudo pacman -S --noconfirm --needed git zsh curl bash-completion
+    chsh -s $(which zsh)
 elif [ -f /etc/debian_version ]; then
     sudo apt install -y git zsh curl
 fi
