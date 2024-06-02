c19ecb9e3c2bfe7e0e47612604e02f23bfd09c25
ogabriel <gabrieloliver8991@gmail.com>
2024-06-02 16:13:28 -0300

    add more dependencies

--- nvim/install.sh
+++ nvim/install.sh
@@ -5,7 +5,15 @@
 # - make (fzf)
 
 if [ -f /etc/arch-release ]; then
-    sudo pacman -S --noconfirm --needed git ripgrep fzf
+    sudo pacman -S --noconfirm --needed \
+        git \
+        ripgrep \
+        fzf \
+        curl \
+        unzip \
+        neovim \
+        tar \
+        gzip
 fi
 
 if [ -d ~/.config/nvim ]; then
