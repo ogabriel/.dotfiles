5c6bbaf7d5ff55872a6c60722c7be82723fdcdc4
ogabriel <gabrieloliver8991@gmail.com>
2024-06-02 15:55:17 -0300

    add new to neovim setup

--- nvim/install.sh
+++ nvim/install.sh
@@ -4,6 +4,10 @@
 # - ripgrep
 # - make (fzf)
 
+if [ -f /etc/arch-release ]; then
+    sudo pacman -S --noconfirm --needed git make ripgrep
+fi
+
 if [ -d ~/.config/nvim ]; then
     rm -rf ~/.config/nvim.bak
     mv ~/.config/nvim ~/.config/nvim.bak
